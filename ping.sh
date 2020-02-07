3f57b18cde933f5aadc407e3d3b004afc07d57c3
Jonathan "Won" Waters <kendoman96@gmail.com>
2020-02-07 15:03:13 -0500

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-cat list.txt | while read output
 
-do
-    ping -c 100 -i .2 "$output" | sed '/^64/ d'>> output.txt
-        echo "=================================" >> output.txt
-    if [ $? -eq 0 ]; then
-            echo "node $output is up"
-    else
-            echo "node $output is down"
-    fi
+main ()
+        {
 
-done
+NUM=$(wc -l list.txt | awk -F 'list.txt' '{print $1}')
+COUNT=0
+
+        while [ $COUNT != $NUM ]; do
+                COUNT=$(($COUNT+1))
+                VAR=$(head -n $COUNT list.txt | tail -1)
+                ping -c 100 -i .2 $VAR | sed '/^64/ d' | sed '/^PING/ d' | sed '/^$/ d' >> output.txt & PIDIOS=$!
+        done
+
+        echo "Running..."
+        wait $PIDIOS
+        echo "Done:"
+
+        cat output.txt
+
+        }
+
+main;
